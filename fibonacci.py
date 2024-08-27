2104032820cc60684a0c96c26ed23c54a5544471
renantito95 <134640159+renantito95@users.noreply.github.com>
2024-08-27 09:20:23 -0300

    SequnciaFibonacci

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+print('*'*30)
+print('Seja bem vindo ao nosso programa em python :)')
+print('*'*30)
+print('Sequencia Fibonacci')
+print('~'*30)
+
+anterior = 0
+proximo = 1
+soma = 1
+
+for n in range(0,31):
+    print(anterior)
+    soma = proximo + anterior
+    anterior = proximo
+    proximo = soma
+
+
+
+
+
+
+
+
+
